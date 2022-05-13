7a92f5acda2830e1e98171821837de6fd4e2c423
AlexOspinaBrain <eiji2l@gmail.com>
2022-05-12 19:17:23 -0500

    bootstrap and transaction 1

--- resources/views/components/transaccion-propia.blade.php
+++ resources/views/components/transaccion-propia.blade.php
@@ -1,7 +1,5 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-    <div>
-        <x-jet-application-logo class="block h-12 w-auto" />
-    </div>
+
 
     <div class="mt-8 text-2xl">
         Vamos a transferir fondos entre tus propias cuentas!
@@ -10,6 +8,30 @@
     <div class="mt-6 text-gray-500">
        <p>Escoje una cuenta origen, luego se habilitaran las cuentas destino.</p>
        <br>
+    </div>
+    <div class="row">
+        <div class="col">
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+        </div>
+        <div class="col">
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+        </div>
+        <div class="col">
+            <input class="form-control" type="number" step="0.01" placeholder="Cantidad transferir..." aria-label="default input example">
+        </div>
+        <div class="col">
+            <button type="button" class="btn btn-success">Transferir</button>
+        </div>
     </div>
 </div>
 
